317ed79352ccc3482791a1a0631735ccd095c3fe
KevinZhou92 <monsterzpc@gmail.com>
2025-11-28 08:04:16 -0800

    Lab 5A Basic

--- src/shardkv1/shardgrp/client.go
+++ src/shardkv1/shardgrp/client.go
@@ -1,31 +1,110 @@
 package shardgrp
 
 import (
+	"sync"
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/shardkv1/shardcfg"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
 type Clerk struct {
 	clnt    *tester.Clnt
 	servers []string
 	// You will have to modify this struct.
+	clientId  string
+	leaderIdx int
+
+	sequenceNum int64
+	mu          sync.Mutex
 }
 
 func MakeClerk(clnt *tester.Clnt, servers []string) *Clerk {
-	ck := &Clerk{clnt: clnt, servers: servers}
+	ck := &Clerk{clnt: clnt, servers: servers, leaderIdx: -1}
 	return ck
 }
 
 func (ck *Clerk) Get(key string) (string, rpc.Tversion, rpc.Err) {
 	// Your code here
-	return "", 0, ""
+	var args rpc.GetArgs
+	var reply rpc.GetReply
+
+	var serverIdx = 0
+	for {
+		if ck.leaderIdx != -1 {
+			serverIdx = ck.leaderIdx
+		} else {
+			serverIdx = (serverIdx + 1) % len(ck.servers)
+		}
+		args = rpc.GetArgs{Key: key}
+		reply = rpc.GetReply{}
+		ok := ck.clnt.Call(ck.servers[serverIdx], "KVServer.Get", &args, &reply)
+		if !ok {
+			// log.Printf("==clerk Get RPC to server %d failed\n", server)
+			continue
+		}
+
+		if reply.Err == rpc.ErrWrongLeader {
+			ck.leaderIdx = -1
+			// log.Printf("==clerk Get RPC to a non leader server %d failed\n", server)
+		}
+
+		if reply.Err == rpc.OK || reply.Err == rpc.ErrNoKey {
+			// log.Printf("==clerk Get RPC to a leader server %d succeeded\n", server)
+			ck.leaderIdx = serverIdx
+			break
+		}
+	}
+
+	return reply.Value, reply.Version, reply.Err
 }
 
 func (ck *Clerk) Put(key string, value string, version rpc.Tversion) rpc.Err {
-	// Your code here
-	return ""
+	// You will have to modify this function.
+	seqNum := ck.getAndIncrementSeqNum()
+	args := rpc.PutArgs{Key: key, Value: value, Version: version, ClientId: ck.clientId, SeqNum: seqNum}
+	var reply rpc.PutReply
+
+	restransmit := false
+	var serverIdx = 0
+	for {
+		if ck.leaderIdx != -1 {
+			serverIdx = ck.leaderIdx
+		} else {
+			serverIdx = (serverIdx + 1) % len(ck.servers)
+		}
+		reply = rpc.PutReply{}
+		// log.Printf("+++clerk Put RPC %v to a server %d with retransmit val %v\n", args, serverIdx, restransmit)
+		ok := ck.clnt.Call(ck.servers[serverIdx], "KVServer.Put", &args, &reply)
+		if !ok {
+			// log.Printf("==clerk Put RPC to server %d failed\n", serverIdx)
+			restransmit = true
+			continue
+		}
+
+		if reply.Err == rpc.ErrWrongLeader {
+			ck.leaderIdx = -1
+			// log.Printf("==clerk Put RPC %v to a non leader server %d failed\n", args, serverIdx)
+			continue
+		}
+
+		if reply.Err == rpc.OK {
+			// log.Printf("==clerk Put RPC %v to a leader server %d succeeded\n", args, serverIdx)
+			ck.leaderIdx = serverIdx
+			break
+		}
+
+		if reply.Err == rpc.ErrVersion {
+			if restransmit {
+				reply.Err = rpc.ErrMaybe
+			}
+			// log.Printf("==clerk Put RPC %v to a leader server %d failed since there is a different version and retransmit is %v\n", args, serverIdx, restransmit)
+			ck.leaderIdx = serverIdx
+			break
+		}
+	}
+	// log.Printf("Put %v took %v", args, dur)
+	return reply.Err
 }
 
 func (ck *Clerk) FreezeShard(s shardcfg.Tshid, num shardcfg.Tnum) ([]byte, rpc.Err) {
@@ -41,4 +120,13 @@ func (ck *Clerk) InstallShard(s shardcfg.Tshid, state []byte, num shardcfg.Tnum)
 func (ck *Clerk) DeleteShard(s shardcfg.Tshid, num shardcfg.Tnum) rpc.Err {
 	// Your code here
 	return ""
+}
+
+func (ck *Clerk) getAndIncrementSeqNum() int64 {
+	ck.mu.Lock()
+	defer ck.mu.Unlock()
+
+	seq := ck.sequenceNum
+	ck.sequenceNum++
+	return seq
 }
--- src/shardkv1/shardgrp/server.go
+++ src/shardkv1/shardgrp/server.go
@@ -1,17 +1,23 @@
 package shardgrp
 
 import (
+	"bytes"
+	"sync"
 	"sync/atomic"
-
 
 	"6.5840/kvraft1/rsm"
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labgob"
 	"6.5840/labrpc"
 	"6.5840/shardkv1/shardgrp/shardrpc"
-	"6.5840/tester1"
+	"6.5840/shardkv1/util"
+	tester "6.5840/tester1"
 )
 
+type VersionedValue struct {
+	Value   string
+	Version rpc.Tversion
+}
 
 type KVServer struct {
 	me   int
@@ -20,36 +26,134 @@ type KVServer struct {
 	gid  tester.Tgid
 
 	// Your code here
-}
+	mu sync.Mutex
 
+	store          map[string]VersionedValue
+	clientsLastSeq map[string]int64
+
+	shardLocked bool
+}
 
 func (kv *KVServer) DoOp(req any) any {
-	// Your code here
-	return nil
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	switch req := req.(type) {
+	case rpc.GetArgs:
+		var reply rpc.GetReply
+
+		// log.Printf("get %v\n", req)
+		val, ok := kv.store[req.Key]
+
+		if !ok {
+			reply.Err = rpc.ErrNoKey
+			return reply
+		}
+
+		reply.Value = val.Value
+		reply.Version = val.Version
+		reply.Err = rpc.OK
+
+		return reply
+	case rpc.PutArgs:
+		var reply rpc.PutReply
+
+		// clientLastSeq, ok := kv.clientsLastSeq[req.ClientId]
+		// if ok && req.SeqNum <= clientLastSeq {
+		// 	log.Printf("$server %d received a duplicate request with seqNum %d, server sequence %d, will not execute it again\n", kv.me, req.SeqNum, clientLastSeq)
+		// 	// This is a duplicate request, return OK without applying again
+		// 	reply.Err = rpc.OK
+		// 	return reply
+		// }
+
+		val, ok := kv.store[req.Key]
+		util.Debug(util.Info, "---server %d put %v while current val is %v with putArgs: %v\n", kv.me, req, val, req)
+		if !ok && req.Version != 0 {
+			reply.Err = rpc.ErrVersion
+			return reply
+		}
+
+		if !ok {
+			val = VersionedValue{Value: "", Version: 0}
+		}
+		if req.Version != val.Version {
+			reply.Err = rpc.ErrVersion
+
+			return reply
+		}
+		val.Value = req.Value
+		val.Version += 1
+		kv.store[req.Key] = val
+		reply.Err = rpc.OK
+		kv.clientsLastSeq[req.ClientId] = req.SeqNum
+
+		return reply
+	default:
+		util.Debug(util.Fatal, "KVServer %d received unknown request type %T", kv.me, req)
+		panic("unknown request type")
+	}
 }
 
-
 func (kv *KVServer) Snapshot() []byte {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
 	// Your code here
-	return nil
+	w := new(bytes.Buffer)
+	e := labgob.NewEncoder(w)
+	e.Encode(kv.store)
+	e.Encode(kv.clientsLastSeq)
+	return w.Bytes()
 }
 
 func (kv *KVServer) Restore(data []byte) {
 	// Your code here
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	// Your code here
+	r := bytes.NewBuffer(data)
+	d := labgob.NewDecoder(r)
+	if d.Decode(&kv.store) != nil {
+		util.Debug(util.Fatal, "%v couldn't decode store", kv.me)
+	}
+	if d.Decode(&kv.clientsLastSeq) != nil {
+		util.Debug(util.Fatal, "%v couldn't decode clientsLastSeq", kv.me)
+	}
 }
 
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 	// Your code here
+	err, res := kv.rsm.Submit(*args)
+	if err != rpc.OK {
+		reply.Err = err
+		return
+	}
+	getReply := res.(rpc.GetReply)
+	reply.Value = getReply.Value
+	reply.Version = getReply.Version
+	reply.Err = getReply.Err
 }
 
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
 	// Your code here
+	err, res := kv.rsm.Submit(*args)
+	if err != rpc.OK {
+		reply.Err = err
+		return
+	}
+	putReply := res.(rpc.PutReply)
+	reply.Err = putReply.Err
 }
 
 // Freeze the specified shard (i.e., reject future Get/Puts for this
 // shard) and return the key/values stored in that shard.
 func (kv *KVServer) FreezeShard(args *shardrpc.FreezeShardArgs, reply *shardrpc.FreezeShardReply) {
 	// Your code here
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	kv.shardLocked = true
+
 }
 
 // Install the supplied state for the specified shard.
@@ -94,7 +198,9 @@ func StartServerShardGrp(servers []*labrpc.ClientEnd, gid tester.Tgid, me int, p
 	labgob.Register(shardrpc.DeleteShardArgs{})
 	labgob.Register(rsm.Op{})
 
-	kv := &KVServer{gid: gid, me: me}
+	store := make(map[string]VersionedValue)
+	clientsLastSeq := make(map[string]int64)
+	kv := &KVServer{gid: gid, me: me, store: store, clientsLastSeq: clientsLastSeq}
 	kv.rsm = rsm.MakeRSM(servers, me, persister, maxraftstate, kv)
 
 	// Your code here
